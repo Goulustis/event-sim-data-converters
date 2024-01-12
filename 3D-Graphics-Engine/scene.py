f4283b93797b9682195ada64b69bf5a4617bbfef
goulustis <cnimaiv@gmail.com>
2024-01-11 17:40:06 -0800

    add carpet scene

--- 3D-Graphics-Engine/scene.py
+++ 3D-Graphics-Engine/scene.py
@@ -47,3 +47,49 @@ class Scene:
     def update(self):
         pass
         # self.moving_cube.rot.xyz = self.app.time
+
+class CarpetScene:
+    def __init__(self, app):
+        self.app = app
+        self.objects = []
+        self.load()
+        # skybox
+        self.skybox = AdvancedSkyBox(app)
+
+    def add_object(self, obj):
+        self.objects.append(obj)
+
+    def load(self):
+        app = self.app
+        add = self.add_object
+
+        # floor
+        tex_dic = {1 : "white",
+                   0 : "black"}
+        tex_idx = 0
+
+        n, s = 20, 2.0
+        for x in np.arange(-n, n + 2, s):
+            for z in np.arange(-n, n + 2, s):
+                # add(Cube(app, pos=(x, -s, z)))
+                add(Cube(app, pos=(x, -s, z), scale=(s/2,s/2,s/2), tex_id="carpet"))
+                tex_idx += 1
+
+        # wall
+        n, s = 20, 1.5
+        tex_idx=1
+        for x in np.arange(-n, n + 3, s):
+            for y in np.arange(-2, n + 3, s):
+                if x < 1:
+                    add(Cube(app, pos=(x, y, -3), scale=(s/2,s/2,s/2), tex_id=tex_dic[tex_idx%2]))
+                else:
+                    add(Cube(app, pos=(x, y, -3), scale=(s/2,s/2,s/2), tex_id="carpet"))
+
+
+                tex_idx += 1
+
+
+
+    def update(self):
+        pass
+        # self.moving_cube.rot.xyz = self.app.time
